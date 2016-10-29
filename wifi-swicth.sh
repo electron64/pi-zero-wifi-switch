35811ba8350791a793602c7095aa696077c91551
ircykk <ircykk@gmail.com>
2016-10-29 13:51:15 +0200

    Second version

--- wifi-swicth.sh
+++ wifi-swicth.sh
@@ -1,31 +1,45 @@
+
 #!/bin/bash
 
-# Ping count
-PING_COUNT=1
-# Ping is installed by default
-PING="/bin/ping"
+# Get Wifi name
+WIFI_SSID=`/sbin/iwgetid -r`
 
-# Test IP address (gogole)
-TEST_IP=8.8.4.4
-# ping test
-$PING -n -c "$PING_COUNT" "$TEST_IP" &>/dev/null
-
-WIFI_SSID=`iwgetid -r`
-
-if [ "$?" = 0 ]
+# Ping to google
+if ping -c 1 8.8.8.8 &> /dev/null
 then
-        if [ "$WIFI_SSID" == "MainWifi" ]
+        # Log info 
+        logger "Network access OK"
+else
+        # Log info 
+        logger "Network DOWN"
+        
+        if [ $WIFI_SSID = "Primary-wifi-name" ]
         then
-                #
+                echo '
+                country=GB
+                ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+                update_config=1
+
+                network={
+                        ssid="Secondary-wifi-name"
+                         psk="Secondary-wifi-password"
+                }' > /etc/wpa_supplicant/wpa_supplicant.conf
         else
-                #
+                echo '
+                country=GB
+                ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+                update_config=1
+
+                network={
+                        ssid="Primary-wifi-name"
+                         psk="Primary-wifi-password"
+                }' > /etc/wpa_supplicant/wpa_supplicant.conf
         fi
 
-        # This will log a message in system log (see manpage).
-        logger "Network access DOWN"
+        # Log info 
+        logger "Restart network"
         
-        # Reboot Pi
+        # Reboot pi
         /sbin/reboot
-else
-    logger "Network access OK"
 fi
+
